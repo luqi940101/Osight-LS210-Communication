bf3a5f955422d63e18e480815e1dac398b842dfe
luqi940101 <luqi940101@gmail.com>
2018-02-08 00:56:25 -0600

    backup

---
+++ Project1/Encapsulation_test.cpp
@@ -0,0 +1,12 @@
+//<pre class = "brush:cpp">
+//#include <iostream>
+//using namespace std;
+//
+//extern"C"
+//{
+//	_declspec(dllexport)int __stdcall testfunc(int a, float b, double c, char d)
+//	{
+//		cout << a << ", " << b << ", " << c << ", " << d << endl;
+//		return 0;
+//	}
+//}
